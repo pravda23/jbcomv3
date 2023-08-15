686321a3f852b36028620b19055a1d3b6b7a8e03
pravda23 <pravda23@gmail.com>
2023-08-15 10:04:45 +0200

    navbar

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -1,23 +1,25 @@
 import "../styles/App.styles.scss";
 import { useRef } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
 
 const Navbar = () => {
   const leftChevron = useRef(null);
   const rightChevron = useRef(null);
-  // const leftChevron = document.querySelector("navbar-chevron-left");
-  // const rightChevron = document.querySelector("navbar-chevron-right");
-
   function goLeft() {
-    console.log("go left");
     leftChevron.current.scrollIntoView({
       behavior: "smooth",
       block: "nearest",
       inline: "center",
     });
+    console.log(leftChevron.current);
   }
   function goRight() {
-    console.log("go right");
+    console.log("move right");
     rightChevron.current.scrollIntoView({
       behavior: "smooth",
       block: "nearest",
@@ -25,57 +27,54 @@ const Navbar = () => {
     });
   }
 
-  // leftChevron.addEventListener("click", goRight);
-  // rightChevron.addEventListener("click", goLeft);
-
   return (
     <div className="navbar-container">
+      <div className="navbar-chevron-left" onClick={goLeft}>
+        <BsFillArrowLeftCircleFill size={20} opacity={0.3} />
+      </div>
       <div className="navbar">
-        <div
-          className="navbar-chevron-left"
-          ref={leftChevron}
-          onClick={goRight}
-        >
-          &lsaquo;
+        <div className="navbar-scroll-anchor-left" ref={leftChevron}></div>
+        <div className="navbar-links-container">
+          <Link to="/writing">
+            <h1 className="btn-nav">WRITING</h1>
+          </Link>
+          <Link to="/audio">
+            {" "}
+            <h1 className="btn-nav">AUDIO</h1>
+          </Link>
+          <Link to="/music">
+            {" "}
+            <h1 className="btn-nav">MUSIC</h1>
+          </Link>
+          <Link to="/contact">
+            {" "}
+            <h1 className="btn-nav">CONTACT</h1>
+          </Link>
+          <Link to="/contact">
+            {" "}
+            <h1 className="btn-nav">CONTACT</h1>
+          </Link>
+          <Link to="/contact">
+            {" "}
+            <h1 className="btn-nav">CONTACT</h1>
+          </Link>
+          <Link to="/contact">
+            {" "}
+            <h1 className="btn-nav">CONTACT</h1>
+          </Link>
+          <Link to="/contact">
+            {" "}
+            <h1 className="btn-nav">CONTACT</h1>
+          </Link>
         </div>
-        <Link to="/writing">
-          <h1 className="btn-nav">WRITING</h1>
-        </Link>
-        <Link to="/audio">
-          {" "}
-          <h1 className="btn-nav">AUDIO</h1>
-        </Link>
-        <Link to="/music">
-          {" "}
-          <h1 className="btn-nav">MUSIC</h1>
-        </Link>
-        <Link to="/contact">
-          {" "}
-          <h1 className="btn-nav">CONTACT</h1>
-        </Link>
-        <Link to="/contact">
-          {" "}
-          <h1 className="btn-nav">CONTACT</h1>
-        </Link>
-        <Link to="/contact">
-          {" "}
-          <h1 className="btn-nav">CONTACT</h1>
-        </Link>
-        <Link to="/contact">
-          {" "}
-          <h1 className="btn-nav">CONTACT</h1>
-        </Link>
-        <Link to="/contact">
-          {" "}
-          <h1 className="btn-nav">CONTACT</h1>
-        </Link>
-        <div
-          className="navbar-chevron-right"
-          ref={rightChevron}
-          onClick={goLeft}
-        >
-          &rsaquo;
-        </div>
+      </div>
+      <div className="navbar-scroll-anchor-right"></div>
+      <div
+        className="navbar-chevron-right"
+        ref={rightChevron}
+        onClick={goRight}
+      >
+        <BsFillArrowRightCircleFill size={20} opacity={0.3} />
       </div>
     </div>
   );
