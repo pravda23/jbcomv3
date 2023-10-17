5f833a7f263ec69492e8cc179e2ea6844d583686
pravda23 <pravda23@gmail.com>
2023-10-17 09:38:30 +0200

    plugging holes in live site

---
+++ src/components/FAQ.component.js
@@ -0,0 +1,16 @@
+const Contact = () => {
+  return (
+    <div className="single-page-container">
+      <div className="hero-title">
+        <h1>Coming soon!</h1>
+        <p className="cta-btn">
+          <a href="https://johnbartmann.com/ratecard.pdf" target="_blank">
+            View the music license ratecard
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
