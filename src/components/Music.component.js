2995996887ece07ce0a84abd1b05ecd38ea0c844
John Bartmann <pravda23@gmail.com>
2024-02-19 11:43:29 +0200

    added external workjobs.js file

--- src/components/Music.component.js
+++ src/components/Music.component.js
@@ -12,24 +12,76 @@ const Music = () => {
   };
 
   const updateTracks = () => {
+    const imgpath = "https:/johnbartmann.com/track-image/800/";
+    const album_slug = [
+      "public-domain-soundtrack-music.jpg",
+      "royalty-free-soundtrack-music.jpg",
+      "making-soundtracks-is-easy.jpg",
+      "audio-drama-soundtracks.jpg",
+      "straylight-drones.jpg",
+      "softly-boldly-with-a-piano.jpg",
+      "90s-tv-sitcom-themes.jpg",
+      "underwater-wonders.jpg",
+      "beachy-beachbox-ukulele.jpg",
+      "happy-trappy.jpg",
+      "picnic-on-the-seine.jpg",
+      "sounds-like-a-dad-band.jpg",
+      "lofi-xmas-sludge.jpg",
+      "hot-equatorial-night.jpg",
+      "chinese-valentine.jpg",
+      "robot-space-rock.jpg",
+      "lime-green-assets.jpg",
+      "western-skies.jpg",
+      "machine-learning.jpg",
+      "eurodisco-party-heaven.jpg",
+      "hide-and-sneak.jpg",
+      "the-moflippin-channel.jpg",
+      "90s-sitcom-themes.jpg",
+    ];
+
+    const album_slug_unpublished = [
+      "free-stuff.jpg",
+      "moody-beats.jpg",
+      "retro-boogie.jpg",
+      "soft-solo-piano.jpg",
+    ];
+
+    let count = 0;
     musicTracks.forEach((t) => {
       // add album title and image
 
+      delete t.albumTitle;
       delete t.img_full;
       delete t.img_thumb;
+      let albumIndex;
 
-      if (t.key <= 20) {
-        t.albumTitle = "public-domain-soundtrack-music";
-        t.img_full = `https:/johnbartmann.com/track-image/800/${t.albumTitle}.jpg`;
+      let key = t.key;
+
+      switch (true) {
+        case key <= count + 20:
+          albumIndex = 0;
+          break;
+        case key <= count + 38:
+          albumIndex = 1;
+          break;
+        case key <= 57:
+          albumIndex = 2;
+          break;
+        case key <= 76:
+          albumIndex = 2;
+          break;
       }
-
-      // t.img_full = `https:/johnbartmann.com/track-image/800/${t.url}.jpg`;
-      // t.img_thumb = `https:/johnbartmann.com/track-image/THUMB_PATH`;
+      t.albumTitle = album_slug[albumIndex];
     });
   };
 
   updateTracks();
-  // console.log(musicTracks[334]);
+  console.log(musicTracks[20].albumTitle);
+  console.log(musicTracks[21].albumTitle);
+  console.log(musicTracks[38].albumTitle);
+  console.log(musicTracks[39].albumTitle);
+  console.log(musicTracks[76].albumTitle);
+  console.log(musicTracks[78].albumTitle);
   console.log(musicTracks);
 
   return (
--- src/components/utility.js
+++ src/components/utility.js
@@ -1,7 +1,17 @@
-import "./musicTracks.json";
+const fs = require("fs");
+const path = require("path");
 
-const result = musicTracks.forEach((t) => {
-  t.img_thumb = "";
+const folderPath = "C:/Users/Guest Account/Desktop/800"; // Replace this with the path to your folder
+
+fs.readdir(folderPath, (err, files) => {
+  if (err) {
+    console.error("Error reading folder:", err);
+    return;
+  }
+
+  const filenamesArray = files.map((file) => `'${file}'`);
+  const copyableArray = `[${filenamesArray.join(", ")}]`;
+
+  console.log("Copyable array of filenames:");
+  console.log(copyableArray);
 });
-
-console.log(musicTracks);
